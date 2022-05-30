40e5d4e56476b0d4b2e953159f3d6052ea90f245
Patka5495 <54615268+Patka5495@users.noreply.github.com>
2022-05-31 04:54:40 +0900

    220531

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,3 @@
-$("#scrollTop").click(function(){
-    console.log("click")
-    $("html, body").animate({scrollTop: 0}, 700);
-});
-
 document.getElementById("nav_name").onclick = function(){
     window.scrollTo({ top: document.getElementById("name_section").offsetTop, behavior: "smooth" });
 }
